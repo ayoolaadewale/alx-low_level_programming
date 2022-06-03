467d062adb400dc5ead8cb7919c4ac7a273d11f3
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-02 18:00:01 -0700

    Script for 100 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - Print out without using print and put
+ *Return: one at the end
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
