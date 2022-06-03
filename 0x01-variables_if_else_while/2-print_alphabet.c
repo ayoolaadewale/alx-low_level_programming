cadb6d5bac59ef72ef858fd301cc27f65ea08a1f
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-03 15:21:21 -0700

    code 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *main - Entry point
+ *Return: Zero at the end
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0);
+	n = rand() - RAND_MAX / 2;
+	for (int i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(tolower(i));
+	}
+	return (0);
+}
