64183bd898233d7d1168b0b1fe7ab794bc41b5ed
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-03 18:24:27 -0700

    9th code

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,15 @@ int main(void)
 {
 	int c;
 
-	for (c = '0'; c < '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		putchar(c);
-		putchar(',');
-		putchar(' ');
+		putchar(c + '0');
+		if (number < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar('9');
 	putchar('\n');
 	return (0);
 }
