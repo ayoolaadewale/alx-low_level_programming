3eb155912d636116bcea6871b61eea7c09ed55b5
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-03 18:50:29 -0700

    code 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point 
+ *Return: zero at the end
+ */
+
+int main(void)
+{
+	int tens;
+	int unit;
+
+	for (tens = 0; tens <= 9; tens ++)
+	{
+		for (unit = tens + 1; unit <= 9; unit++)
+		{
+			putchar(tens + '0');
+			putchar(unit + '0');
+
+			if (tens < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
