9791ff0a87216c7c47ec7b20cb79777551502b84
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-06 06:46:53 -0700

    Script for 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,32 +9,16 @@ int main(void)
 {
 	int tens;
 	int unit;
-	int tent;
-	int unitt;
-
-	for (tens = 0; tens <= 9; tens++)
+	
+	for (tens = 0; tens < 100; tens++)
 	{
-		for (unit = 0; unit <= 9; unit++)
+		for (unit = tens + 1; unit < 100; unit++)
 		{
-			for (tent = tens; tent <= 9; tent++)
-			{
-				for (unitt = unit + 1; unitt <= 9; unitt++)
-				{
-					putchar(tens + '0');
-					putchar(unit + '0');
-					putchar(' ');
-					putchar(tent + '0');
-					putchar(unitt + '0');
-
-					if (!((tens == 9 && unit == 8) && (tent == 9 && unitt == 9)))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				unitt = 0;
-			}
-
+			putchar(tens / 10 + '0');
+			putchar(tens % 10 + '0');
+			putchar(' ');
+			putchar(unit / 10 + '0');
+			putchar(unit % 10 + '0');
 		}
 	}
 	putchar('\n');
