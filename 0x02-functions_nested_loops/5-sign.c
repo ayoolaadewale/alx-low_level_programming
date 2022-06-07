1ee9e8e6ff15c551f32d5c7b9757a64af558d253
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-07 16:36:53 -0700

    last changes to 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,18 +2,18 @@
 
 /**
  *print_sign - Entry point
- *n: Parameter to be checked
- *Retun: Values to be returned based on outputs
+ *@n: Parameter to be checked
+ *Return: Values to be returned based on outputs
  */
 
-int print_sign(int n)
+int print_aign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if ( n == 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
@@ -21,6 +21,6 @@ int print_sign(int n)
 	else
 	{
 		_putchar('-');
-		return (-1);
+		return ('-1');
 	}
 }
