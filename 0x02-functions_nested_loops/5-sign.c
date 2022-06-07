2c6e0ae11f64e3bed39e1571aaffe8dc5958525c
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-07 16:31:42 -0700

    script 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_sign - Entry point
+ *n: Parameter to be checked
+ *Retun: Values to be returned based on outputs
+ */
+
+int print_aign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if ( n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return ('-1');
+	}
+}
