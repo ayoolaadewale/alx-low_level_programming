0b7b0db330349e75aa1cf600da49cb57e5814489
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-07 17:29:49 -0700

    script for 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *add -entry point
+ *@r: parameter to be added
+ *@q: paramter to be added
+ *Return: value of added nunbers
+ */
+
+int add(int r, int q)
+{
+	return (r + q);
+}
