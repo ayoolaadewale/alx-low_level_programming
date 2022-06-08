2c2ba0a160d2bfacbdeff5098249fe9bdca26c0a
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-07 17:15:10 -0700

    script 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *print_last_digit - Entry point
+ *@r: parameter to be checked
+ *Return: value to be returned
+ */
+
+int print_last_digit(int r)
+{
+	return ((abs(r) % 10));
+}
