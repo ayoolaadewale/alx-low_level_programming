a6360fb1a8e8a8987dccc450a9e452f7c186ca0f
ayoolaadewale <ayoolaadewale@yahoo.com>
2022-06-09 11:20:11 +0000

    changes to script 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ *main - Entry point
+ *Return: Zero at the end
+ */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
 
 	return (0);
