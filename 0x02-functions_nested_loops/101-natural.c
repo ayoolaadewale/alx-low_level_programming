0629cdd0776a9a697168b5b83d1f2e61da9a5236
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-08 20:00:17 -0700

    script for 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Zero at the end
+ */
+
+int main(void)
+{
+	int n, value = 0;
+
+	for (n = 1; n < 1024; n ++)
+	{
+		if (n % 3 ==0)
+		{
+			value = value + n;
+		}
+		if (n % 5 == 0)
+		{
+			value = value + n;
+		}
+	}
+	printf("%d", value);
+	return (0);
+}
