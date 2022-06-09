b363a758d196af24c295f59c48a3c0e53edcff36
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-08 19:15:47 -0700

    script for 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ *print_times_table - Entry point
+ *@n: Parameter to be checked
+ */
+
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	if (n >= 0 && n <=15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = (i * j);
+
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+
+					if (k <= 9)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(k + '0');
+					}
+					else if (k > 9 && k < 100)
+					{
+						_putchar(' ');
+						_putchar((k / 10) + '0');
+						_putchar((k % 10) + '0');
+					}
+					else if (k > 100)
+					{
+						_putchar((k / 100) + '0');
+						_putchar((k / 10) % 10 + '0');
+						_putchar((k % 10) + '0');
+					}
+				}
+			}
+			_putchar('\n');
+
+		}
+	}
+}
