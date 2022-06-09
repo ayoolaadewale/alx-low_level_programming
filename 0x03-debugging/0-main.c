2629ee921c508cc213cdaad086463848fc2ea84e
ayoolaadewale <ayoolaadewale@yahoo.com>
2022-06-09 21:47:16 +0000

    script 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *main - Entry point
- *Return: Zero at the end
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
@@ -13,4 +13,5 @@ int main(void)
 	positive_or_negative(i);
 	
 	return (0);
+
 }
