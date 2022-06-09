3bddd9e8b4afa256c1204d1cf66c43ff3e8450cf
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-08 18:36:09 -0700

    changes to script9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,14 +23,14 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(product + '0');
+				_putchar(k + '0');
 			}
 			else if (k > 9)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
