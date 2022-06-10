b8a13a45bf9ba77a2600763530e847a3c60d1e99
ayoolaadewale <ayoolaadewale@yahoo.com>
2022-06-10 01:06:35 +0000

    script for 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *mul - Entry point
+ *@a: Parameter 1
+ *@b: Paramter 2
+ *Return: Product
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return(product);
+}
