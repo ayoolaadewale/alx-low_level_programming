8d1060d00b847ec10c3542ad357bbf9424c40de1
ayoolaadewale <ayoolaadewale@yahoo.com>
2022-06-10 02:02:33 +0000

    script 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *more_numbers - Entry point
+ */
+
+void more_numbers(void)
+{
+	int c, d;
+
+	for (c = 0; c < 10; c++)
+	{
+		for (d = 0; d <= 14; d++)
+		{
+			if (d > 9)
+			{
+				_putchar(d / 10 + '0');
+				_putchar(d % 10 + '0');
+			}
+			else
+			{
+				_putchar(d + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
