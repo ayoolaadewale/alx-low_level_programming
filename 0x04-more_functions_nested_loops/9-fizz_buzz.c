3d1bd4c23688ad9dc22799e2b686fd13750bf16b
ayoolaadewale <ayoolaadewale@yahoo.com>
2022-06-10 13:31:04 +0000

    script 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,54 +11,23 @@ int main(void)
 
 	for (c = 1; c <= 100; c++)
 	{
-		if (c <= 9)
+		if (c % 15 == 0)
 		{
-			if (c % 3 == 0 && c % 5 == 0)
-			{
-				printf("FizzBuzz");
-				putchar(' ');
-			}
-			else if (c % 5 == 0)
-			{
-				printf("Buzz");
-				putchar(' ');
-			}
-			else if (c % 3 == 0)
-			{
-				printf("Fizz");
-				putchar(' ');
-			}
-			else
-			{
-				putchar(c + '0');
-				putchar(' ');
-			}
+			printf("FizzBuzz ");
+		}
+		else if (c % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (c % 3 == 0)
+		{
+			printf("Fizz ");
 		}
 		else
 		{
-			if (c % 3 == 0 && c % 5 == 0)
-			{
-				printf("FizzBuzz");
-				putchar(' ');
-			}
-			else if (c % 5 == 0)
-			{
-				printf("Buzz");
-				putchar(' ');
-			}
-			else if (c % 3 == 0)
-			{
-				printf("Fizz");
-				putchar(' ');
-			}
-			else
-			{
-				putchar(c + '0');
-				putchar(' ');
-			}
+			printf("%d ", c);
 		}
 	}
-	putchar('\n');
-
+	printf("\n");
 	return (0);
 }
