7f514043e82c3636c8c6196689d01dc801ebf462
ayoolaadewale <ayoolaadewale@yahoo.com>
2022-06-10 12:24:59 +0000

    script 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,15 +9,15 @@ void print_square(int size)
 {
 	int c, d;
 
-	if (n <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (c = 0; c < n; c++)
+		for (c = 0; c < size; c++)
 		{
-			for (d = 0; d < n; d++)
+			for (d = 0; d < size; d++)
 			{
 				_putchar('#');
 			}
