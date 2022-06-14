4793063004eaef3d76e5a6793fd4bfe4ca4af6cf
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-14 15:40:57 -0700

    script for 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strlen - Entry point
+ *@s: Parameter
+ *Return: Length of array
+ */
+
+int _strlen(char *s)
+{
+	int array_length;
+
+	array_length = strlen(s);
+	return (array_length);
+}
