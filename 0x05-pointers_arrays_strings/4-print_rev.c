a359a23b2c633c9e427e1d6745cfacfcf98ebb38
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-14 18:10:19 -0700

    script 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_rev - Entry point
+ *@s: Parameter
+ */
+
+void print_rev(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	for (len = len - 1; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}
