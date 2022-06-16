713ca19df8c9653549c70374f17b3fcad7e303e4
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-16 12:36:25 +0000

    script 3

---
+++ 0x06-pointers_arrays_strings/strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_strcmp - Entry point
+ *@s1: parameter one
+ *@s2: parameter two
+ *Return: Zero
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
