6d5869b87ab003e48791d9d6442211e20b6717c7
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-16 12:01:16 -0700

    canges to header

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -5,3 +8,5 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a; int n);
 char *string_toupper(char *n);
+
+#endif
