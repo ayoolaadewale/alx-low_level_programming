cb2aa293dbc9539ce17921be0316d2129055c2cf
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-20 11:24:17 +0000

    script 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_memcpy - Entry point
+ *@dest: Parameter 
+ *@src: parameter
+ *@n: parameter
+ *Return: value
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (n > 0)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+		n--;
+	}
+	return (dest);
+}
