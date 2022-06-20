f46fd6f98352daac6114d64f595bae9de072ad38
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-20 11:44:38 +0000

    script 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ *_strchr - Entry point
+ *@s: parameter
+ *@c: parameter
+ *Return: value
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	
+	for (i = 0; s[i] = '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
