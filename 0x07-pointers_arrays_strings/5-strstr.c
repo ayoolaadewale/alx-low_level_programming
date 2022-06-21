a5bc17f6027ab4aa7329e6adaa3e346beaf5828c
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-21 12:01:15 +0000

    script 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *_strstr - entry point
+ *@haystack: first arg
+ *@needle: second arg
+ *Return: value
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *h = haystack;
+	char *n = needle;
+
+	while (*h)
+	{
+		n = needle;
+		h = haystack;
+		while (*n)
+		{
+			if (*h == *n)
+			{
+				n++;
+				h++;
+			}
+			else
+				break;
+		}
+		if (*n == '\0')
+			return (haystack);
+		haystack++;
+	}
+	return (0);
+}
