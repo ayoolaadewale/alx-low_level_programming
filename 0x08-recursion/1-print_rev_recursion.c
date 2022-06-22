c00ce164c531974282b70b8e7ff6efd2ea457b63
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-22 12:07:20 +0000

    script 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - entry point
+ *@s: argument
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (s* == '\0')
+	{
+		_putchar('\n');
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
