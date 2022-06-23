b979583c1f01215f923480d97197e4f3dd52214c
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-23 10:33:50 +0000

    script 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *find_prime - Entry point for first function
+ *@n: argument one
+ *@x: argument two
+ *Return: value
+ */
+
+int find_prime(int n, int x)
+{
+	x = 2;
+
+	if (n % x != 0)
+		return 1;
+	if (n % x == 0)
+		return 0;
+	return (find_prime(n, x + 1));
+}
+
+/**
+ *is_prime_number - main function to check if n is prime or not
+ *@n: value to be checked
+ *Return: 1 or 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return 0;
+	return (find_prime(n, 2));
+}
