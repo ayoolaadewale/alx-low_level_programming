e371da6d4bc72a108fed63e8accdf6a3ff4b6c46
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-26 16:41:20 +0000

    script 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - entry point
+ *@argc: argument count
+ *@argv: array of argument
+ *Return: zero
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("\%s\.\n". *argv);
+	return (0);
+}
