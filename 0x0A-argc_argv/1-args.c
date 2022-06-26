3435ddd8b42954d02d31b960f55302915d50003e
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-26 16:54:24 +0000

    script 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main -entry pint
+ *@argc: argument count
+ *@argv: array of args
+ *Return: zero
+ */
+
+int main(int argc, char *agrv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
