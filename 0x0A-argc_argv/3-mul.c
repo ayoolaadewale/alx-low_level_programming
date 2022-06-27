5c0d45af1a425c8f8fa37cb88b5d8d6fa6de0f88
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-27 10:44:36 +0000

    script 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,24 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  *main - entry point
+ *@argc: count of args
+ *@argv: array of args
+ *Return: zero
  */
+
+int main(int argc, char *argv[])
+{
+	int count;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	count = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", count);
+	return (0);
+}
