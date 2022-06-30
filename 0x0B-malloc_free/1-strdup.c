fd78540554405d0bc198b012d5c8cd6a85bf2b69
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-30 02:33:51 +0000

    script 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - entry point
+ *@str: arg one
+ *Return: pointer to array or null
+ */
+
+char *_strdup(char *str)
+{
+	char *stup;
+	int i, j;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	i++;
+	stup = malloc(sizeof(*str) * i);
+	if (stup == NULL)
+		return (NULL);
+	j = 0;
+	while (str[j] != '\0')
+	{
+		stup[j] = strp[j];
+		j++
+	}
+	return (stup);
+}
