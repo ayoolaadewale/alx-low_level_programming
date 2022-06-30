f78a999aa16938201e69f539ff33816fb8d3f362
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-30 03:12:41 +0000

    script 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,8 +22,8 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 		{
-			while (--1 >= 0)
-				free(grid[i];
+			while (--i >= 0)
+				free(grid[i]);
 			free(grid);
 			return (NULL);
 		}
