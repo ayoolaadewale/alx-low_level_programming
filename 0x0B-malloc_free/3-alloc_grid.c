7631d94023dc9390e920a3cfb48da657c6baf31c
ayoolaadewale <ayooladewale@yahoo.com>
2022-06-30 02:59:00 +0000

    script 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *alloc_grid - entry point
+ *@width: arg one
+ *@height: arg two
+ *Return: pointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid, i, j;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			while (--1 >= 0)
+				free(grid[i];
+			free(grid);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++;)
+	{
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;	
+	}
+	return (grid);
+}
