30e4572e3f1bffdb4c3a2d450b7088641cdee683
ayoolaadewale <ayooladewale@yahoo.com>
2022-07-01 01:02:33 +0000

    script 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *malloc_checker - Entry point
+ *@b: arg one
+ *Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *pp;
+
+	pp = malloc(b);
+
+	if ( pp == NULL)
+		exit(98);
+
+	return (pp);
+}
