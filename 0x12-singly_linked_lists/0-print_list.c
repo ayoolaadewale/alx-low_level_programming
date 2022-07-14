e6489053ff66a32e3e3f7939cc5a0b545cbddced
ayoolaadewale <ayooladewale@yahoo.com>
2022-07-14 13:33:40 +0000

    script 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *print_list - entry point
+ *@h: arg one
+ *Return: number of nodes in linked list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return(i);
+}
