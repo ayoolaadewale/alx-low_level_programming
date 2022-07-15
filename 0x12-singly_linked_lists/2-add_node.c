b21c53bf84c2a32bd5827da2ba69b44500a7582d
ayoolaadewale <ayooladewale@yahoo.com>
2022-07-15 02:25:27 +0000

    script 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,51 @@
 #include "lists.h"
 
 /**
- *
+ *_strlen - get length of the string
+ *@s: string to be checked
+ *Return: length of the string
+ */
+
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ *add_node - entry point
+ *@head: current position
+ *@str: string to add
+ *Return: pointer to current position
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int i, len;
+	char *content;
+	list_t *new;
+
+	if (str == NULL || head == NULL)
+		return (NULL);
+	len = _strlen(str);
+	new = *head;
+	content = malloc((len + 1) * sizeof(char));
+	if (content == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		content[i] = str[i];
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		free(content);
+		return (NULL);
+	}
+	new->str = content;
+	new->len = len;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
