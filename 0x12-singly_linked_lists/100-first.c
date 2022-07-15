7073d1bc954bfb1cc1dc7ff941889377e1224bed
ayoolaadewale <ayooladewale@yahoo.com>
2022-07-15 02:14:43 +0000

    script 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,5 +7,5 @@
 void __attribute__((constructor)) execute_before_main()
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("I before my house upon my back!\n");
+	printf("I bore my house upon my back!\n");
 }
