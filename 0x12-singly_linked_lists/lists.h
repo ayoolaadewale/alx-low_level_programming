5498a7a09fa1f4ac82c146eeffa1b521a85133e7
ayoolaadewale <ayooladewale@yahoo.com>
2022-07-15 01:31:28 +0000

    script 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,8 +8,8 @@
 
 /**
  *strcut list_s -singly linked list
- *@str: string- (malloc'ed string)
- *@len: length of string
+ *@str: string - (malloc'ed string)
+ *@len: length of the string
  *@next: points to the next node
  *
  *Description: singly linked list node structure
