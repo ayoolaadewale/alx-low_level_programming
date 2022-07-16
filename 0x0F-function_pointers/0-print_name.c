5d3ac449ea9e34bf2bfaa604e8b27f9cd7cdca09
ayoolaadewale <ayooladewale@yahoo.com>
2022-07-16 18:34:01 +0000

    script 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ *print_name - entry point
+ *@name: string to be printed
+ *@f: function to print string
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
