42d5ee2d5959553abbd689a74135f69959b46349
ayoolaadewale <ayooladewale@yahoo.com>
2022-07-22 01:12:26 +0000

    script 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *claer_bit - entry point
+ *@n: arg one
+ *@index: arg two
+ *Return: one or minus one
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	*n = *n & ~(1 << index);
+	return ((index > 8 * sizeof(*n)) ? -1 : 1);
+}
