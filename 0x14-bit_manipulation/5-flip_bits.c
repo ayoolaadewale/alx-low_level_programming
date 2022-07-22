e564b607fea477cc2c7f687336904cf8dcfce319
ayoolaadewale <ayooladewale@yahoo.com>
2022-07-22 01:30:26 +0000

    script 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *flip_bits - entry point 
+ *flip_bits - entry point
  *@n: arg one
  *@m: arg two
  *Return: number of bit transformation
@@ -12,7 +12,6 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	int i, res;
 
 	res = 0;
-
 	for (i = 8 * sizeof(n) - 1; i >= 0; i--)
 		if (((n ^ m) >> i) & 1)
 			res++;
