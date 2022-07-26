67a116d8a018a3faac91ef71c66c7be123d5cf8b
ayoolaadewale <ayooladewale@yahoo.com>
2022-07-26 03:14:49 +0000

    script 5

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ *main - entry point
+ *@ac: arg one
+ *@av: arg two
+ *Return: one or zero
+ */
+
+int main (int ac, char *av[])
+{
+	return (1);
+	return (0);
+}
