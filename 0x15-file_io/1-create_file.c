caecedede2b8bba5412486bc2de558e199b90890
ayoolaadewale <ayooladewale@yahoo.com>
2022-07-26 02:45:57 +0000

    script 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ *create_file - entry point
+ *@filename: file to be created
+ *@text_content: content of the file to be created
+ *Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, length;
+	ssize_t res_write;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		length = 0;
+		while (*(text_content + length) != '\0')
+			length++;
+		res_write = write(fd, text_contentm length);
+		if (res_write == -1)
+		{
+			write(1, "fails", 6)
+				return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
