5da3cb9f81e149bdcc55f9e5be26c1c91f9754f6
ayoolaadewale <ayooladewale@yahoo.com>
2022-09-03 04:23:00 +0000

    script 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		if (index == size)
 		return (tmp);
 		size++;
-		tmp = tmp->nex;
+		tmp = tmp->next;
 	}
 	return (NULL);
 }
